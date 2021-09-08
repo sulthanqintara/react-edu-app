a4fb5d17177292300db0c925d88a2bbf1943e3d9
sulthanqintara <sulthanqintara@gmail.com>
2021-09-08 14:49:54 +0700

    Initate repo for react web

---
+++ src/pages/App.js
@@ -0,0 +1,11 @@
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <Route path="/" exact></Route>
+    </Router>
+  );
+}
+
+export default App;
