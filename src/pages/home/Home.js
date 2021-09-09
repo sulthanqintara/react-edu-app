271186192e9d8f56e5b5c85ecb5bf68b05e44d15
Candra Sidik Dermawan <projectcsd07@gmail.com>
2021-09-09 14:42:10 +0700

    add page home & notification

---
+++ src/pages/home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from "../../components/navbar/Navbar";
+
+function Home() {
+    return (
+        <> 
+        <Navbar/>
+            
+        </>
+    )
+}
+
+export default Home
