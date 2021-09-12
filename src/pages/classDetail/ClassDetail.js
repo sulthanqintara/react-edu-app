316cefaa04ac921be2ab028b2b9c1758616b4b37
Candra Sidik Dermawan <projectcsd07@gmail.com>
2021-09-12 11:33:50 +0700

    fix notif  class student

--- src/pages/classDetail/ClassDetail.js
+++ src/pages/classDetail/ClassDetail.js
@@ -12,16 +12,14 @@ function ClassDetail() {
   return (
     <>
       <main>
-        <section className="classDetail-page">
-          <div className="d-flex flex-column flex-xl-row">
-            <div className="col-md-2 col-6 flex-grow-1">
-              <Navbar className=" flex-grow-1" />
-            </div>
-            <div className="col-md-9 col-6 content-activity ">
+        <section className="classDetail-page vh-100">
+          <div className="d-flex flex-column flex-xl-row justify-content-between vh-100">
+            <Navbar />
+            <div className="col-md-9 col-6 content-activity pt-1">
               <div className="container container-fluid">
                 <div className="row">
                   <div className="col-1">
-                    <Link>
+                    <Link to="/myclass">
                       <img
                         src={iconBack}
                         alt=""
@@ -29,20 +27,20 @@ function ClassDetail() {
                       />
                     </Link>
                   </div>
-                  <div className="col-6">
+                  <div className="col-6 mb-3">
                     <h4>Know more Javacript</h4>
                   </div>
                   <div className="col-12">
-                    <div class="card">
-                      <img src={headerImage} class="card-img-top" alt="..." />
-                      <div class="card-body">
+                    <div className="card">
+                      <img src={headerImage} className="card-img-top" alt="..." />
+                      <div className="card-body">
                         <div className="row">
                           <div className="col-4 text-center icon-computer">
                             <img
                               src={iconComputer}
                               alt=""
                               className="img-fluid"
-                              //   style={{ marginTop: "-30vh" }}
+                            //   style={{ marginTop: "-30vh" }}
                             />
                           </div>
                           <div className="col-5 description-classDetail">
@@ -79,54 +77,56 @@ function ClassDetail() {
                             <hr />
                           </div>
                         </div>
-                        <div className="row description-information">
-                          <div className="col-4 ">
-                            <h4 className="text-center ml-5 ">Description</h4>
+                        <div className="desc-scroll">
+                          <div className="row description-information">
+                            <div className="col-4 ">
+                              <h4 className="text-center ml-5 ">Description</h4>
+                            </div>
+                            <div className="col-12">
+                              <p>
+                                Javascript from the basic for beginner. JavaScript
+                                is a programming language that adds interactivity
+                                to your website. This happens in games, in the
+                                behavior of responses when buttons are pressed or
+                                with data entry on forms; with dynamic styling;
+                                with animation, etc. This class helps you get
+                                started with JavaScript and furthers your
+                                understanding of what is possible.
+                              </p>
+                            </div>
                           </div>
-                          <div className="col-12">
-                            <p>
-                              Javascript from the basic for beginner. JavaScript
-                              is a programming language that adds interactivity
-                              to your website. This happens in games, in the
-                              behavior of responses when buttons are pressed or
-                              with data entry on forms; with dynamic styling;
-                              with animation, etc. This class helps you get
-                              started with JavaScript and furthers your
-                              understanding of what is possible.
-                            </p>
+                          <div className="row description-learn">
+                            <div className="col-4 ">
+                              <h4 className="text-center ">What will i learn</h4>
+                            </div>
+                            <div className="col-12">
+                              <ul>
+                                <li>
+                                  Lorem ipsum dolor sit amet consectetur
+                                  adipisicing elit. Numquam necessitatibus
+                                  repudiandae tenetur iusto eaque, id vero! At
+                                  earum illo unde omnis laborum quae, molestiae
+                                  numquam excepturi vel iste dicta perspiciatis!
+                                </li>
+                              </ul>
+                            </div>
                           </div>
-                        </div>
-                        <div className="row description-learn">
-                          <div className="col-4 ">
-                            <h4 className="text-center ">What will i learn</h4>
-                          </div>
-                          <div className="col-12">
-                            <ul>
-                              <li>
-                                Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Numquam necessitatibus
-                                repudiandae tenetur iusto eaque, id vero! At
-                                earum illo unde omnis laborum quae, molestiae
-                                numquam excepturi vel iste dicta perspiciatis!
-                              </li>
-                            </ul>
-                          </div>
-                        </div>
-                        <div className="row description-content">
-                          <div className="col-4 ">
-                            <h4 className="text-center  ">Content 2</h4>
-                          </div>
-                          <div className="col-12">
-                            <ul>
-                              <li>
-                                Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Blanditiis ex nesciunt magni
-                                rerum libero eaque et ab quaerat expedita
-                                veritatis. Praesentium earum soluta distinctio
-                                veniam optio, consequatur ducimus maiores
-                                itaque?
-                              </li>
-                            </ul>
+                          <div className="row description-content">
+                            <div className="col-4 ">
+                              <h4 className="text-center  ">Content 2</h4>
+                            </div>
+                            <div className="col-12">
+                              <ul>
+                                <li>
+                                  Lorem ipsum dolor sit amet consectetur
+                                  adipisicing elit. Blanditiis ex nesciunt magni
+                                  rerum libero eaque et ab quaerat expedita
+                                  veritatis. Praesentium earum soluta distinctio
+                                  veniam optio, consequatur ducimus maiores
+                                  itaque?
+                                </li>
+                              </ul>
+                            </div>
                           </div>
                         </div>
                       </div>
