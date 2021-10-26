d215b469f4864b05ed540c1c28544c9e2b2510b0
sulthanqintara <sulthanqintara@gmail.com>
2021-10-26 15:57:53 +0700

    eslint profile

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from "react";
 import "../profile/profile.css";
 import Navbar from "../../components/navbar/Navbar";
