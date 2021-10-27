988942f91492ad47446500b6c800a5b60999894e
unknown <2018014949b.ctc@gmail.com>
2021-10-27 12:19:29 +0700

    add create class

--- src/pages/fasilitatorClass/FasilitatorClass.js
+++ src/pages/fasilitatorClass/FasilitatorClass.js
@@ -1,108 +1,202 @@
-import React from 'react'
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
 import MyClass from "../../components/fasilitatorMyClass/FasilitatorMyClass";
 import Navbar from "../../components/navbar/Navbar";
-import "./fasilitatorClass.css"
-import { Link } from 'react-router-dom';
+import "./fasilitatorClass.css";
+import { useSelector } from "react-redux";
+import { createClass } from "../../utils/https/classes";
+// import { Link } from "react-router-dom";
 
 function FasilitatorClass() {
-    return (
-        <>
-            <main className="fasilitator d-flex vh-100">
-                <Navbar />
-                <section className="container-fluid my-class d-flex flex-column pt-4 pb-4 pe-5 ps-5 ms-4 me-5">
-                    <div className="mb-2">
-                        <h1>Activity</h1>
-                        <h2>My Class</h2>
-                        <div className="container-fluid table-class ps-4">
-                            <div className="header-table d-flex justify-content-start ps-3">
-                                <Form.Check aria-label="option 1" />
-                                <h3 className="col-2 text-start">Class Name</h3>
-                                <h3 className="col-2">Category</h3>
-                                <h3 className="col-4 text-start">Description</h3>
-                                <h3 className="col-2">Schedule</h3>
-                                <h3 className="col-1">Students</h3>
-                            </div>
-                            <MyClass />
-                            <MyClass />
-                            <MyClass />
-                            <h6 className="text-center mt-3">view all {'>'}</h6>
-                        </div>
+  const faciliator_id = useSelector((state) => state.auth.authInfo.user_id);
+  const [className, setClassName] = useState("");
+  const [category, setCategory] = useState("");
+  const [pricing, setPricing] = useState("");
+  const [level, setLevel] = useState("");
+  const [schedule, setSchedule] = useState("");
+  const [date, setDate] = useState("");
+  const [startTime, setStartTime] = useState("");
+  const [endTime, setEndTime] = useState("");
+  const [description, setDescription] = useState("");
+
+  const onCreate = () => {
+    const data = new URLSearchParams();
+    data.append("name", className);
+    data.append("category_id", category);
+    data.append("description", description);
+    data.append("day", schedule);
+    data.append("level_id", level);
+    data.append("start_date", date);
+    data.append("start_time", startTime);
+    data.append("end_time", endTime);
+    data.append("facilitator_id", faciliator_id);
+    data.append("pricing", pricing);
+
+    createClass(data)
+      .then((res) => console.log(res))
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <>
+      <main className="fasilitator d-flex vh-100">
+        <Navbar />
+        <section className="container-fluid my-class d-flex flex-column pt-4 pb-4 pe-5 ps-5 ms-4 me-5">
+          <div className="mb-2">
+            <h1>Activity</h1>
+            <h2>My Class</h2>
+            <div className="container-fluid table-class ps-4">
+              <div className="header-table d-flex justify-content-start ps-3">
+                <Form.Check aria-label="option 1" />
+                <h3 className="col-2 text-start">Class Name</h3>
+                <h3 className="col-2">Category</h3>
+                <h3 className="col-4 text-start">Description</h3>
+                <h3 className="col-2">Schedule</h3>
+                <h3 className="col-1">Students</h3>
+              </div>
+              <MyClass />
+              <MyClass />
+              <MyClass />
+              <h6 className="text-center mt-3">view all {">"}</h6>
+            </div>
+          </div>
+          <div className="bd-new-class mt-4">
+            <h2>Create New class</h2>
+            <div className="container-fluid create-class ps-5 pt-4 pe-5 ms-4 me-5">
+              <form>
+                <div className="d-flex mb-4">
+                  <div className="col-6">
+                    <div className="input-name d-flex mb-3">
+                      <p>Class Name</p>
+                      <span>:</span>
+                      <input
+                        type="text"
+                        value={className}
+                        onChange={(e) => setClassName(e.target.value)}
+                      />
                     </div>
-                    <div className="bd-new-class mt-4">
-                        <h2>Create New class</h2>
-                        <div className="container-fluid create-class ps-5 pt-4 pe-5 ms-4 me-5">
-                            <form>
-                                <div className="d-flex mb-4">
-                                    <div className="col-6">
-                                        <div className="input-name d-flex mb-3">
-                                            <p>Class Name</p>
-                                            <span>:</span>
-                                            <input type="text" />
-                                        </div>
-                                        <div className="input-categories d-flex">
-                                            <p>Categories</p>
-                                            <span>:</span>
-                                            <select className="form-select" aria-label="Default select example">
-                                                <option defaultValue>Open this select menu</option>
-                                                <option value="1">Software</option>
-                                                <option value="2">History</option>
-                                                <option value="3">Finance</option>
-                                                <option value="4">Sains</option>
-                                                <option value="5">Math</option>
-                                                <option value="6">Science</option>
-                                            </select>
-                                        </div>
-                                    </div>
-                                    <div className="col-6">
-                                        <div className="input-price d-flex">
-                                            <p>Pricing</p>
-                                            <span>:</span>
-                                            <Form.Group className="mb-2 d-flex" id="formGridCheckbox">
-                                                <Form.Check type="radio" label="Free" />
-                                                <Form.Check type="radio" label="Paid" />
-                                            </Form.Group>
-                                        </div>
-                                        <div className="input-schedule d-flex">
-                                            <p>Schedule</p>
-                                            <span>:</span>
-                                            <select className="form-select" aria-label="Default select example">
-                                                <option defaultValue>Open this select menu</option>
-                                                <option value="1">Monday</option>
-                                                <option value="2">Tuesday</option>
-                                                <option value="3">Wednesday</option>
-                                                <option value="4">Thursday</option>
-                                                <option value="5">Friday</option>
-                                                <option value="6">Saturday</option>
-                                                <option value="7">Sunday</option>
-                                            </select>
-                                            <input type="time" className="me-2" />
-                                            <input type="time" />
-                                        </div>
-                                    </div>
-                                </div>
-                                <div className="input-description">
-                                    <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                                        <Form.Label>Description</Form.Label>
-                                        <Form.Control as="textarea" rows={3} />
-                                    </Form.Group>
-                                </div>
-                                <div className="text-end">
-                                    <Link to="/fasilitator/class-detail">
-                                        <Button className="text-right" variant="success">Create</Button>
-                                    </Link>
-                                </div>
-
-                            </form>
-                        </div>
+                    <div className="input-categories d-flex">
+                      <p>Categories</p>
+                      <span>:</span>
+                      <select
+                        className="form-select"
+                        aria-label="Default select example"
+                        onChange={(e) => setCategory(e.target.value)}
+                        value={category}
+                      >
+                        <option defaultValue>Open this select menu</option>
+                        <option value="1">Software</option>
+                        <option value="2">History</option>
+                        <option value="3">Psychology</option>
+                        <option value="4">Finance</option>
+                        <option value="5">Math</option>
+                      </select>
                     </div>
-                </section>
-
-
-            </main>
-        </>
-    )
+                    <div className="input-categories d-flex mt-4">
+                      <p>Level</p>
+                      <span>:</span>
+                      <select
+                        className="form-select"
+                        aria-label="Default select example"
+                        onChange={(e) => setLevel(e.target.value)}
+                        value={level}
+                      >
+                        <option defaultValue>Open this select menu</option>
+                        <option value="1">Beginner</option>
+                        <option value="2">Intermediate</option>
+                        <option value="3">Advanced</option>
+                      </select>
+                    </div>
+                  </div>
+                  <div className="col-6">
+                    <div className="input-schedule d-flex mt-4">
+                      <p>Schedule</p>
+                      <span>:</span>
+                      <select
+                        className="form-select"
+                        aria-label="Default select example"
+                        value={schedule}
+                        onChange={(e) => setSchedule(e.target.value)}
+                      >
+                        <option defaultValue>Open this select menu</option>
+                        <option value="1">Monday</option>
+                        <option value="2">Tuesday</option>
+                        <option value="3">Wednesday</option>
+                        <option value="4">Thursday</option>
+                        <option value="5">Friday</option>
+                        <option value="6">Saturday</option>
+                        <option value="7">Sunday</option>
+                      </select>
+                      <input
+                        type="time"
+                        className="me-2"
+                        value={startTime}
+                        onChange={(e) => setStartTime(e.target.value)}
+                      />
+                      <input
+                        type="time"
+                        value={endTime}
+                        onChange={(e) => setEndTime(e.target.value)}
+                      />
+                    </div>
+                    <div className="input-name d-flex mt-4">
+                      <p>Start Date </p>
+                      <span>:</span>
+                      <input
+                        type="date"
+                        value={date}
+                        onChange={(e) => setDate(e.target.value)}
+                      />
+                    </div>
+                    <div className="input-categories d-flex mt-4">
+                      <p>Pricing</p>
+                      <span>:</span>
+                      <select
+                        className="form-select"
+                        aria-label="Default select example"
+                        onChange={(e) => setPricing(e.target.value)}
+                        value={pricing}
+                      >
+                        <option defaultValue>Open this select menu</option>
+                        <option value="0">Free : Rp. 0</option>
+                        <option value="60000">Paid : Rp. 60.000</option>
+                        <option value="40000">Paid : Rp. 40.000</option>
+                        <option value="80000">Paid : Rp. 80.000</option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <div className="input-description">
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlTextarea1"
+                  >
+                    <Form.Label>Description</Form.Label>
+                    <Form.Control
+                      as="textarea"
+                      rows={3}
+                      value={description}
+                      onChange={(e) => setDescription(e.target.value)}
+                    />
+                  </Form.Group>
+                </div>
+                <div className="text-end">
+                  <Button
+                    className="text-right"
+                    variant="success"
+                    onClick={onCreate}
+                  >
+                    Create
+                  </Button>
+                </div>
+              </form>
+            </div>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
-export default FasilitatorClass
+export default FasilitatorClass;
